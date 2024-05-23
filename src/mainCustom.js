a934617a34aa0ce996485e17ce24c86999c7752c
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2024-05-22 23:23:48 -0600

    Revert "test styles import"
    This reverts commit 6f22ad5eaff05d882c346cfae6ed802a64892751.

--- src/mainCustom.js
+++ src/mainCustom.js
@@ -1,5 +1,6 @@
 import card from './js/components/card.js';
 import customResult from './js/customResults.js';
+import './styles/style.css';
 
 const plantObj = JSON.parse(sessionStorage.getItem('plantObj'));
 card(plantObj, 'card__container');
