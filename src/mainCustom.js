12ab587e78a6cdab70ee504973a4289bcdca1f07
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2024-05-23 22:03:14 -0600

    modify styles and add them to html

--- src/mainCustom.js
+++ src/mainCustom.js
@@ -1,6 +1,5 @@
 import card from './js/components/card.js';
 import customResult from './js/customResults.js';
-import './styles/style.css';
 
 const plantObj = JSON.parse(sessionStorage.getItem('plantObj'));
 card(plantObj, 'card__container');
