ff687beb3047b9d976da7910506ac2de735a438a
Heylin-Loaiza <heylinloaiza.s@gmail.com>
2024-05-23 22:31:45 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'docs'),
   },
   devtool: 'inline-source-map',
